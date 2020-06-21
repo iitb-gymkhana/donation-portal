b7f8589026de5f01a098755cec00e5b9e2b08b46
nautatva <nnautatva@gmail.com>
2020-06-21 19:54:45 +0530

    meta-changes

--- index.php
+++ index.php
@@ -24,22 +24,22 @@ require_once 'sso_connect.php';
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps." />
+  <meta name="description" content="Let's join together and help our mess, nteen laundry, stationary shop helpers and fellow students during these trouble-some times." />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
   <title>Student Donation Drive</title>
 
   <!-- Add to homescreen for Chrome on Android -->
   <meta name="mobile-web-app-capable" content="yes" />
-  <link rel="icon" sizes="192x192" href="images/android-desktop.png" />
+  <link rel="icon" sizes="192x192" href="images/icon-196.png">
 
   <!-- Add to homescreen for Safari on iOS -->
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-  <meta name="apple-mobile-web-app-title" content="Material Design Lite" />
-  <link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png" />
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="Material Design Lite">
+  <link rel="apple-touch-icon-precomposed" href="images/icon-256.png">
 
   <!-- Tile icon for Win8 (144x144 + tile color) -->
-  <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png" />
+  <meta name="msapplication-TileImage" content="images/icon-196.png" />
   <meta name="msapplication-TileColor" content="#3372DF" />
 
   <link rel="shortcut icon" href="images/favicon.png" />
